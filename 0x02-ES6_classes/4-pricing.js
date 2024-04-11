ebd903d0f1895874335a96304451325f25f1d7ec
hamza chankour <hamzachanik@outlook.com>
2024-04-11 09:08:16 -0700

    task

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,46 @@
+import Currency from './3-currency';
+
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new Error('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new Error('Currency must be an instance of Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new Error('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new Error('Conversion rate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
