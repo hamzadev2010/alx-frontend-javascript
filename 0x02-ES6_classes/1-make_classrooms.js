36eef8d31372ccf58de36208fe6dcf3490ce9be9
hamza chankour <hamzachanik@outlook.com>
2024-04-11 08:03:50 -0700

    task

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,10 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-	return [
-    new Classroom(19),
-    new Classroom(20),
-    new Classroom(34),
-  ];
+
+ return [
+    new ClassRoom(19),
+    new ClassRoom(20), 
+    new ClassRoom(34)
+	 ];
 }
