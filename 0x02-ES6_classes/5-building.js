066f5e2480305d2af2fa19a5915571143be839ad
hamza chankour <hamzachanik@outlook.com>
2024-04-11 13:31:18 -0700

    task

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,22 +1,28 @@
-export default class Building {
+class Building {
   constructor(sqft) {
+    this.sqft = sqft;
     if (this.constructor === Building) {
+      throw new Error(
+        'Abstract class Building cannot be instantiated directly.'
+      );
+    }
+    if (typeof this.evacuationWarningMessage !== 'function') {
       throw new Error(
         'Class extending Building must override evacuationWarningMessage'
       );
     }
-    this.sqft = sqft;
-  }
-
-  set sqft(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = value;
+  }
 }
 
+export default Building;
