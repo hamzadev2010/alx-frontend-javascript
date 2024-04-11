3fbd0c5e90a5a638350c434683d416c221e42429
hamza chankour <hamzachanik@outlook.com>
2024-04-11 13:23:01 -0700

    tas

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,23 +1,22 @@
 export default class Building {
-    constructor(sqft) {
-    this.sqft = sqft;
-    if (this.constructor !== Building) {
-      if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error (
-          'Class extending Building must override evacuationWarningMessage',
-        );
-      }
+  constructor(sqft) {
+    if (this.constructor === Building) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage'
+      );
     }
+    this.sqft = sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  set sqft(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new Error('sqft must be a number');
-    }
-	   this._sqft = sqft;
-  }
 }
+
