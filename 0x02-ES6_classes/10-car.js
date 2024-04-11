74f7074bb8a437246635047861794cc9524decce
hamza chankour <hamzachanik@outlook.com>
2024-04-11 13:03:50 -0700

    task

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,42 @@
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Spc = this.constructor[Symbol.species];
+    return new Spc(this._brand, this._motor, this._color);
+  }
+}
+
+export default Car;
