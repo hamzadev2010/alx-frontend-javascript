bc74b0f4b14ac4468ae2189d0ddb90432d925c9a
hamza chankour <hamzachanik@outlook.com>
2024-04-11 08:45:43 -0700

    task

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -5,6 +5,9 @@ class HolbertonCourse {
     this._students = students;
   }
 
+   /**
+   * @parametre of string name
+   */
   set name(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
@@ -12,16 +15,22 @@ class HolbertonCourse {
     this._name = name;
   }
 
-  set length(length) {
+   /**
+   * @parametre of lentgh of the numbers
+   */
+   set length(length) {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
   }
 
-  set students(students) {
+   /**
+   * @parametre of students
+   */
+   set students(students) {
     if (!Array.isArray(students) || !students.every(s => typeof s === 'string')) {
-      throw new TypeError('Students must be an array of strings');
+      throw new TypeError('Students must be an Array');
     }
     this._students = students;
   }
