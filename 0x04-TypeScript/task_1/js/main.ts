a83bd12947bbf84c1db04342d1ab8511a3a051be
hamza chankour <hamzachanik@outlook.com>
2024-04-17 07:58:12 -0700

    task

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,38 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly fullTimeEmployee: boolean;
+  readonly yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any; 
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements Student {
+  firstName: string;
+  lastName: string;
+
+
+
+constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+ }
+  }
+
+export default Teacher;
