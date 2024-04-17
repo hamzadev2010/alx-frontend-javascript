a91dae1afdd75ff6ae11f1c5d8a3ebce2e3936a9
hamza chankour <hamzachanik@outlook.com>
2024-04-17 09:45:38 -0700

    task

---
+++ 0x04-TypeScript/task_4/js/subjects/main.ts
@@ -0,0 +1,16 @@
+//path="subjects/Teacher.ts  and subjects/Cpp.ts subjects/React.ts
+
+console.log("C++");
+cpp.setTeacher = cTeacher;
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log("Java");
+java.setTeacher = cTeacher;
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log("React");
+react.setTeacher = cTeacher;
+console.log(react.getRequirements());
+console.log(react.getAvailableTea
