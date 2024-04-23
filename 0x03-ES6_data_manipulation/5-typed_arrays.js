93d6504468f9d696af810dc7a2460ca792be8f6e
hamza chankour <hamzachanik@outlook.com>
2024-04-23 04:35:01 -0700

    task

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,10 +2,9 @@ export default function createInt8TypedArray(length, position, value) {
   const buf = new ArrayBuffer(length);
   const dt = new DataView(buf);
 
+// chekinge the position	
   if (position >= length || position < 0) { throw new Error('Position outside range');
-  } else {
-    dt.setInt8(position, value);
+  } else {dt.setInt8(position, value);
   }
-
   return dt;
 };
