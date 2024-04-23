f91398e70d63f1228ccbb1af3a1e9e074d7b84c0
hamza chankour <hamzachanik@outlook.com>
2024-04-23 06:54:35 -0700

    task

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+// function named cleanSet that returns a string of
+// all the set values that start with a specific string (startString).
+export default function cleanSet() {
+  const maps = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+  return maps;
+}
