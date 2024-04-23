144be4d205fe48894884d0bb5e984f8e81d30c81
hamza chankour <hamzachanik@outlook.com>
2024-04-23 07:01:05 -0700

    task

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,9 @@
-// function named cleanSet that returns a string of
-// all the set values that start with a specific string (startString).
-export default function cleanSet() {
-  const maps = new Map([
-    ['Apples', 10],
-    ['Tomatoes', 10],
-    ['Pasta', 1],
-    ['Rice', 1],
-    ['Banana', 5],
-  ]);
-  return maps;
-}
+const cleanSet = (set, startString) => {
+  const resultArray = Array.from(set)
+    .filter((item) => typeof item === 'string' && item.startsWith(startString))
+    .map((item) => item.slice(startString.length));
+
+  return resultArray.join('-');
+};
+
+export default cleanSet;
