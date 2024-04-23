bdd8d79f60a2239f773866abee508101249f893b
hamza chankour <hamzachanik@outlook.com>
2024-04-23 05:29:17 -0700

    task

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+function createGroceriesList() {
+  return new Map([
+    ['Milk', 2],
+    ['Eggs', 12],
+    ['Bread', 1],
+    ['Cheese', 1],
+    ['Yogurt', 4],
+  ]);
+}
+
+export default createGroceriesList;
