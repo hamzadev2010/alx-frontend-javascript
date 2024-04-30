47c04a70525977c5687a9f5b44e90f9930481047
hamza chankour <hamzachanik@outlook.com>
2024-04-30 11:42:46 -0700

    task

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,9 @@
+// a function named divideFunction that will accept two arguments:
+// numerator (Number) and denominator (Number).
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  } else {
+    return numerator / denominator;
+  }
+}
