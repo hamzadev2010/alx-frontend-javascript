9bd8250808c9220283ead62de9e2f08c4461a4ec
hamza chankour <hamzachanik@outlook.com>
2024-04-30 11:24:44 -0700

    task

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+// function named handleProfileSignup. It should accept three arguments firstName (string),
+//lastName (string), and fileName (string). The function should call the two other functions.
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((rs) => {
+    const resu = [];
+    for (const element of rs) {
+      resu.push({ status: element.status, value: element.value || element.reason });
+    }
+    return resu;
+  });
+}
