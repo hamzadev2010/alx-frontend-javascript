1742b0089c2e05d128a4e7b6a34fc4fd43e41b6e
hamza chankour <hamzachanik@outlook.com>
2024-04-30 11:30:44 -0700

    task

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,7 +4,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName),
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]).then((rs) => {
     const resu = [];
